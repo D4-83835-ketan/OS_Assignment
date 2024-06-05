8a28a33239cf36b2ceee9ddba3dee5180531e91a
Bhideketan <bhideketan3@gmail.com>
2024-06-05 21:41:21 +0530

    Assignment 5 que7 is added

---
+++ Assignment5/q7.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter number : "
+read num
+
+if [ $num -gt 0 ]
+then 
+echo "Entered number $num is a POSITIVE number"
+else
+echo "Entered number $num is a NIGATIVE number"
+fi
+
